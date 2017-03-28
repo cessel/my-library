007e4c8338961b5d168fee9aeefb1da0a0362829
cessel <cessel@yandex.ru>
2017-03-28 10:44:51 +0300

    файл с js

---
+++ js/misc.js
@@ -0,0 +1,36 @@
+/* »нициализаци€ янкдекс  арт */
+ymaps.ready(init);
+	var myMap, 
+		myPlacemark;
+
+		function init()
+			{
+				var adress = '<? echo $contacts['adress']; ?>';
+	            ymaps.geocode(adress).then(function (res)
+					{
+						var position = res.geoObjects.get(0).geometry.getCoordinates();
+    					myMap = new ymaps.Map('map',{center: position,zoom : 14 });
+						var myPlacemark = new ymaps.Placemark(position,{hintContent: '–усска€ Ўкола »скусств',balloonContent: '–усска€ Ўкола »скусств - ћосква, ”л. Ћ€пидевского, д. 10а'});
+						myMap.geoObjects.add(myPlacemark);  
+					}); 
+			
+			}
+
+/* Проверки */
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+    }
+function isValidPhone(myPhone) { 
+   return /^\d{11}$/.test(myPhone); 
+} 
+
+function alertObj(obj) { 
+    var str = ""; 
+    for(k in obj) { 
+        str += k+": "+ obj[k]+"\r\n"; 
+    } 
+    alert(str); 
+} 
+
